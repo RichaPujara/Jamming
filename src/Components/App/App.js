bd29a088a720605df1168844c08c3ec956069fd0
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-08-09 13:21:15 +1000

    spotify search added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import SearchBar from "../SearchBar/SearchBar.js";
 import SearchResults from "../SearchResults/SearchResults.js";
 import PlayList from "../Playlist/Playlist.js";
+import Spotify from "../../util/Spotify";
 
 class App extends React.Component {
   constructor(props) {
@@ -83,7 +84,7 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResult => this.setState({searchResults: searchResult}))
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+
+
 const clientId = process.env.CLIENT_ID
 const redirectURI = 'http://localhost:3000/';
 let accessToken;
@@ -14,13 +16,37 @@ const Spotify = {
 
         if (accessTokenExpiry && accessTokenFromUrl) {
             accessToken = accessTokenFromUrl[1];
-            expiryTime = accessTokenExpiry[1];
+            const expiryTime = accessTokenExpiry[1];
             window.setTimeout(() => accessToken = '', expiryTime * 1000);
             window.history.pushState('Access Token', null, '/')
             return accessToken;
         } else {
             window.location = `https://api.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
         }
+    },
+    search(term) {
+        const accessToken = Spotify.getAccessToken()
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }).then(response => response.json())
+            .then(jsonResponse => {
+                if (!jsonResponse) {
+                    return []
+                }
+                jsonResponse.map(track => {
+                    return {
+                        ID: track.id,
+                        Name: track.name,
+                        Artist: track.artists[0].name,
+                        Album: track.album.name,
+                        URI:track.uri
+                    }
+                
+                })
+        })
+        
     }
 }
 
